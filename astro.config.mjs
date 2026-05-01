d80f636f99745798f5681f08bc039abce43a71a0
anjunt87 <your.email@example.com>
2026-05-01 09:23:54 +0700

    fix: resolve merge before deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,11 @@ import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  devToolbar: { enabled: false },
+
   output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  }),
-
-  integrations: [
-    react()
-  ],
-
+  adapter: node({ mode: 'standalone' }),
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   }
